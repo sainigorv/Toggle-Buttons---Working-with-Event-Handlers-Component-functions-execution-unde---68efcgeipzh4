eee2abaea4e6e93abfc02e6ce0afe78441e723b2
sainigorv <sainigorv341@gmail.com>
2023-01-07 04:01:40 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,31 @@
-import React, {useState} from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 const App = () => {
-//code here 
-  
+  const [Data1, setData1] = useState("OFF");
+  const [Data2, setData2] = useState("ON");
 
-  
+  function handleClick(event) {
+    if (event.target.id === "btn1") {
+      setData1("ON");
+      setData2("OFF");
+    } else {
+      setData1("OFF");
+      setData2("ON");
+    }
+  }
+
   return (
     <div className="App">
-      <button id="btn1" onClick={handleClick}>
-      {/* assign value for button 1 */}
+      <button id="btn1" onClick={(e) => handleClick(e)}>
+        {Data1}
       </button>
       <br />
       <br />
-      <button id="btn2" onClick={handleClick}>
-        {/* assign value for button 2 */}
+      <button id="btn2" onClick={(e) => handleClick(e)}>
+        {Data2}
       </button>
     </div>
   );
-}
-
+};
 
 export default App;
